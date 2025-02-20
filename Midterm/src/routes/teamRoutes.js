d9a93e01141892850c608c957021c2c9d87d51e9
Akhtar Ali <ali.akhtar1271@gmail.com>
2025-02-20 18:28:03 -0500

    Add route for getting teams by city

--- Midterm/src/routes/teamRoutes.js
+++ Midterm/src/routes/teamRoutes.js
@@ -5,6 +5,9 @@ const teamController = require('../controllers/teamController');
 // Get all teams
 router.get('/', teamController.getAllTeams);
 
+// Get teams by city
+router.get('/city/:city', teamController.getTeamsByCity);
+
 // Get team by ID
 router.get('/:id', teamController.getTeamById);
 
