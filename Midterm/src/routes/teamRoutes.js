30193356c1fcdd89d8cdd56f02f7179a03a9f89e
Akhtar Ali <ali.akhtar1271@gmail.com>
2025-02-20 18:27:20 -0500

    Add route for getting team by ID

--- Midterm/src/routes/teamRoutes.js
+++ Midterm/src/routes/teamRoutes.js
@@ -5,4 +5,7 @@ const teamController = require('../controllers/teamController');
 // Get all teams
 router.get('/', teamController.getAllTeams);
 
+// Get team by ID
+router.get('/:id', teamController.getTeamById);
+
 module.exports = router;
